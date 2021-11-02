b8fbe79cec1da9dbb5a0f00dc83069c24faab10c
root <root@DESKTOP-MPF6NS4.localdomain>
2021-11-02 16:22:35 +0700

    pert6

--- pert6/source1.cpp
+++ pert6/source1.cpp
@@ -1,5 +1,21 @@
-#include <iostream> 
-using namespace std;  
-int main(){     
-    cout <<"hello world";     
-    return 0; }
+#include <iostream>
+using namespace std;
+void Fibonaci (int N)
+{
+ long fib0=0, fib1=1, fib;
+ cout<<"Nilai Fibonaci "<<fib0<<" ";
+ while (fib0<= N/2)
+{
+ fib=fib0+fib1;
+ fib0=fib1;
+ fib1=fib;
+ cout<<fib1<<" ";
+}
+}
+int main (){
+ int N;
+ long hasil;
+ cout<<"Masukan batas akhir dari bilangan fibonaci : ";cin>>N;
+ Fibonaci(N);
+ cout<<endl;
+}
