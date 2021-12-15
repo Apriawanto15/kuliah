6b2a0d66e41feb2c6d002d057f983c14529b4bca
root <root@DESKTOP-MPF6NS4.localdomain>
2021-12-15 16:38:55 +0700

    pert11

---
+++ pert11/fun.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+void fname(){
+    cout << "Feri";
+}
+
+void mname(){
+    cout << "Apriawanto";
+}
+
+void lname(){
+    cout << "";
+}
+
+void spasi(){
+    cout<<" ";
+}
+
+int main() {
+    fname();
+    spasi();
+    mname();
+    spasi();
+    lname();
+    return 0;
+}
